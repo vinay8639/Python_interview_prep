4448587b8e12869485ce768e96b0f986265b814b
vinay8639 <77714268+vinay8639@users.noreply.github.com>
2024-11-11 19:54:18 +0530

    Q2 added

--- Leet_code_set1.py
+++ Leet_code_set1.py
@@ -13,3 +13,21 @@ def twoSum(nums, target):
         if comp in set_dict:
             return [num, set_dict[comp]]
         set_dict[i] = num
+
+
+"""
+Q2: Maximum Subarray
+Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+Output: 6
+Explanation: The subarray [4,-1,2,1] has the largest sum 6.
+"""
+
+nums = [2, 3, -8, 7, -1, 2, 3]
+max_current = max_global = nums[0]
+for num in nums[1:]:
+    max_current = max(num, max_current + num)
+    
+    if max_current > max_global:
+        max_global = max_current
+        
+print(max_global)
