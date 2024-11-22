132a374d82fee986a20720bf63c18cfa9f26c881
vinay8639 <77714268+vinay8639@users.noreply.github.com>
2024-11-22 19:27:32 +0530

    Q10 added

--- Leet_code_set1.py
+++ Leet_code_set1.py
@@ -181,3 +181,24 @@ def rotate( nums: List[int], k: int) -> None:
     if k > 0:
         k %= num_count
         nums[:] = nums[-k:] + nums[:-k]
+
+"""
+Q10 Best Time to Buy and Sell Stock II
+Input: prices = [7,1,5,3,6,4]
+Output: 7
+Explanation: Buy on day 2 (price = 1) and sell on day 3 (price = 5), profit = 5-1 = 4.
+Then buy on day 4 (price = 3) and sell on day 5 (price = 6), profit = 6-3 = 3.
+Total profit is 4 + 3 = 7.
+"""
+
+def maxProfit(prices: List[int]) -> int:
+    profit = 0
+
+    for i in range(1, len(prices)):
+        sale = prices[i] - prices[i - 1]
+
+        if sale > 0:
+            profit += sale
+
+    return profit
+    
