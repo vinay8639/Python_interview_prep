3e5145c5ec8f22902704b09f6541fb43ff79f3a4
vinay8639 <77714268+vinay8639@users.noreply.github.com>
2024-11-22 17:43:35 +0530

    Q8 added

--- Leet_code_set1.py
+++ Leet_code_set1.py
@@ -1,3 +1,4 @@
+from typings import List, dict
 """
 Q1. Two Sum
 
@@ -143,3 +144,24 @@ def removeDuplicates(nums):
             index += 1
     
     return index
+
+"""
+Q8 Majority Element
+Input: nums = [2,2,1,1,1,2,2]
+Output: 2
+"""
+
+def majorityElement(nums):
+    count = 0
+    candidate = 0
+    
+    for num in nums:
+        if count == 0:
+            candidate = num
+        
+        if num == candidate:
+            count += 1
+        else:
+            count -= 1
+    
+    return candidate
